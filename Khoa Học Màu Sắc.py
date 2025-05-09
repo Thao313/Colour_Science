08eb640c8a37840fd99ea95b48f77de72f871690
Thao611 <vobethao93@gmail.com>
2025-05-09 14:28:36 +0700

    Add files via upload

---
+++ Khoa Học Màu Sắc.py
@@ -0,0 +1,17 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Khoa Học Màu Sắc", layout="wide"
+)
+
+st.write("## Chào mừng bạn đến với project Khoa Học Màu Sắc của nhóm mình! ")
+st.write('### Mình tên là Thảo, MSSV 22158088')
+st.write('## Bạn làm nhóm với mình tên là Lưu Đức Hải My, MSSV 22158070')
+st.markdown(
+    """
+    <style>
+    [data-testid="stAppViewContainer"] {
+        background-color: #FDF8E2; /* màu be */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True)
