8f490c0a74d25d017f5cdbd036ae12312f92b860
Thao611 <vobethao93@gmail.com>
2025-05-09 14:29:48 +0700

    1Chuyển đổi Không gian Màu.py

--- pages/1Chuyển đổi Không gian Màu.py
+++ pages/1Chuyển đổi Không gian Màu.py
@@ -1,1 +1,63 @@
+import streamlit as st
+import numpy as np
+import colour 
+st.set_page_config(
+    page_title="Khoa Học Màu Sắc", layout="wide"
+)
 
+st.write('# Chuyển đổi không gian màu')
+
+Chon_khong_gian_mau = st.sidebar.radio("Chuyển không gian màu", ("sRGB to XYZ", "XYZ to sRGB", "XYZ to Lab"))
+#sidebar: để ở tay trái
+col1,col2 = st.columns(2) #chia màn hình thành 2 cột
+if Chon_khong_gian_mau == 'sRGB to XYZ':
+    with col1:
+        st.write("sRGB to XYZ")
+        R = st.number_input('Nhập R:')
+        G = st.number_input('Nhập G:')
+        B = st.number_input('Nhập B:')
+        if st. button ('Chuyển sang XYZ'):
+            R = R/255.0
+            G = G/255.0
+            B = B/255.0
+            RGB = np.array([R, G, B])
+            XYZ = colour.sRGB_to_XYZ(RGB)
+            X = XYZ[0]*100
+            Y = XYZ[1]*100
+            Z = XYZ[2]*100
+            s = '### X = %.2f, Y = %.2f, Z = %.2f' %(X, Y, Z)
+            st.write(s)
+elif Chon_khong_gian_mau == "XYZ to sRGB":
+    with col1:
+        st.write("### XYZ to sRGB")
+        X = st.number_input('Nhập X:')
+        Y = st.number_input('Nhập Y:')
+        Z = st.number_input('Nhập Z:')
+        if st. button ('Chuyển sang sRGB'):
+            X = X/100.0
+            Y = Y/100.0
+            Z = Z/100.0
+            XYZ = np.array([X, Y, Z])
+            RGB = colour.XYZ_to_sRGB(XYZ)
+            R = RGB[0]*255
+            G = RGB[1]*255
+            B = RGB[2]*255
+            s = '### R = %.0f, G = %.0f, B = %.0f' %(R, G, B)
+            st.write(s)
+elif Chon_khong_gian_mau == "XYZ to Lab":
+    with col1:
+        st.write("### XYZ to Lab")
+        X = st.number_input('Nhập X:')
+        Y = st.number_input('Nhập Y:')
+        Z = st.number_input('Nhập Z:')
+        if st. button ('Chuyển sang Lab'):
+            X = X/100.0
+            Y = Y/100.0
+            Z = Z/100.0
+            XYZ = np.array([X, Y, Z])
+            Lab = colour.XYZ_to_Lab(XYZ)
+            L = Lab[0]
+            a = Lab[1]
+            b = Lab[2]
+            s = '### L = %.2f, a = %.2f, b = %.2f' %(L, a, b)
+            st.write(s)
